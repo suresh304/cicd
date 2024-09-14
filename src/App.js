fb1643b641bf13b72e07601f18ab67de2c32cff8
AlliSuresh-ZLR <suresh.a@zelarsoft.com>
2024-09-14 16:57:59 +0530

    ghjk

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
       <header className="App-header">
         
           Learn React
-          <h2>Carpe diem.........slack alerts</h2>
+          <h2>Carpe diem....kill ego</h2>
       </header>
     </div>
   );
