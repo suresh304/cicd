26c1682a5a70b5cbaa10f06dfd8a6a704059599d
AlliSuresh-ZLR <suresh.a@zelarsoft.com>
2024-09-16 14:46:15 +0530

    testing

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
 
-          chicken dinner srujan
+          chicken dinner srujan and suresh
       </header>
     </div>
   );
